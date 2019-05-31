38c2edbb69c8a6bfce588ec07a599288a7ca786a
Eric Blincow <eblincow@barracuda.com>
2019-05-30 22:59:48 -0700

    Finished 1-12

--- 1-12.c
+++ 1-12.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+#define OUT 0
+#define IN 1
+
 int main () {
 
-    int c;
+    int c, STATE;
+    STATE = 0;
     while ((c = getchar()) != EOF) {
-        putchar(c);
-
+       if (c=='\t' || c==' ' || c=='\n') { 
+            STATE = OUT;
+            continue;
+        } else if (STATE == OUT) {
+            STATE = IN;
+            putchar('\n');
+        }
+       putchar(c);
     }
-
-
 }
